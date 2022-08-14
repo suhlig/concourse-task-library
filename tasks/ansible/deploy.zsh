3431a3805cad6d35b4621b58f5af9f65846fd9ba
Steffen Uhlig <steffen@familie-uhlig.net>
2022-08-14 11:57:02 +0200

    Read inventory using Ansible instead of interpreting it

--- tasks/ansible/deploy.zsh
+++ tasks/ansible/deploy.zsh
@@ -3,16 +3,41 @@
 set -e
 
 main() {
+  configure-ansible
+  configure-vault-password
+  configure-inventory
+  configure-extra-vars
   install-roles
   configure-ssh
-  write-extra-vars
-  write-vault-password
-  configure-inventory
   run-play
 }
 
-install-roles() {
+configure-ansible() {
   export ANSIBLE_CONFIG="$PLAYBOOK_PATH"/ansible.cfg
+}
+
+configure-vault-password() {
+  echo "$VAULT_PASSWORD" > .vault_password
+}
+
+configure-inventory() {
+  if [ -f "$PLAYBOOK_PATH"/"$INVENTORY" ]; then
+    # It's a file
+    export ANSIBLE_INVENTORY=="$PLAYBOOK_PATH"/"$INVENTORY"
+  else
+    # It's a string; assume list of hosts. We append a comma in order to make it a list, even if there is only one host.
+    export ANSIBLE_INVENTORY="$INVENTORY",
+  fi
+}
+
+configure-extra-vars() {
+  # jinja2-cli does not accept the template from STDIN, otherwise this could become a single
+  # jinja2 -D pwd="$PWD" - <<<"$EXTRA_VARS" | tee extra-variables.yml
+  echo "$EXTRA_VARS" > extra-variables.j2.yml
+  jinja2 -D pwd="$PWD" extra-variables.j2.yml -o extra-variables.yml
+}
+
+install-roles() {
   ansible-galaxy install --role-file "$PLAYBOOK_PATH"/requirements.yml
 }
 
@@ -21,35 +46,20 @@ configure-ssh() {
   echo "$SSH_KEY" > ~/.ssh/id_rsa
   chmod 600 ~/.ssh/id_rsa
 
-  # Trust all servers public keys
-  yq eval '.all.hosts.*.public_key' deployment/inventory.yml >> ~/.ssh/known_hosts
-}
-
-write-extra-vars() {
-  # jinja2-cli does not accept the template from STDIN, otherwise this could become a single
-  # jinja2 -D pwd="$PWD" - <<<"$EXTRA_VARS" | tee extra-variables.yml
-  echo "$EXTRA_VARS" > extra-variables.j2.yml
-  jinja2 -D pwd="$PWD" extra-variables.j2.yml -o extra-variables.yml
-}
-
-write-vault-password() {
-  echo "$VAULT_PASSWORD" > .vault_password
-}
-
-configure-inventory() {
-  if [ -f "$PLAYBOOK_PATH"/"$INVENTORY" ]; then
-    # It's a file; assume YAML.
-    INVENTORY="$PLAYBOOK_PATH"/"$INVENTORY"
-  else
-    # It's a string; assume single host. We append a comma in order to make it a list.
-    INVENTORY="$INVENTORY",
-  fi
+  # Trust all servers' public keys.
+  # Defaults to the empty string if not present.
+  ansible-inventory \
+    --list \
+    --playbook-dir "$PLAYBOOK_PATH" \
+    --extra-vars @extra-variables.yml \
+    --vault-password-file .vault_password \
+  | yq eval '._meta.hostvars.*.public_key // ""' - \
+  >> ~/.ssh/known_hosts
 }
 
 run-play() {
   ansible-playbook \
-    "$PLAYBOOK_PATH"/playbook.yml \
-    --inventory "$INVENTORY" \
+    --playbook-dir "$PLAYBOOK_PATH" \
     --extra-vars @extra-variables.yml \
     --vault-password-file .vault_password
 }
